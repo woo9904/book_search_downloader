212f3420710518ea05d95b658da55f8628427156
woo9904 <skwjddnwls12@naver.com>
2022-07-22 16:06:46 +0900

    code modified

--- book_downloader.py
+++ book_downloader.py
@@ -68,7 +68,7 @@ def do_search():
     #책 검색
     options=webdriver.ChromeOptions()
     options.add_argument("widow-size=1920*1080")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36") #유저 에이전트 확인 필요
+    options.add_argument() #add your user-agent
 
     #이방법은 통합 exe파일을 만들기 위한 방법
     # if chkvar.get()==0:
